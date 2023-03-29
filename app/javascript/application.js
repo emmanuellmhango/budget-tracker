c66780210036ae09ede960d71ad561a66aefeff6
emmanuel mhango <emmanuellmhango@gmail.com>
2023-03-29 21:50:46 +0400

    finalize splash screen

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-const menu = document.getElementById('menu');
-
-menu.addEventListener('click', () => {
-  const menuContainer = document.querySelector('.menu-container');
-  menuContainer.style.display === 'none' ? (menuContainer.style.display = 'block') : (menuContainer.style.display = 'none');
-});
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "categories#index"
+  root "splash#index"
+  devise_scope :user do
+    authenticated :user do
+      root to: 'categories#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root to: 'splash#index', as: :unauthenticated_root
+    end
+  end
 end
